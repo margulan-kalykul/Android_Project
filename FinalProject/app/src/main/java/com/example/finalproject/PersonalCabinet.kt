d3957d521d8d414b299f5646f2dd516b691f4946
Az1tchhh <98698562+Az1tchhh@users.noreply.github.com>
2023-04-03 23:20:45 +0600

    added activities

---
+++ FinalProject/app/src/main/java/com/example/finalproject/PersonalCabinet.kt
@@ -0,0 +1,13 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalCabinet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_cabinet)
+
+
+    }
+}
---
+++ FinalProject/app/src/main/java/com/example/finalproject/Product.kt
@@ -0,0 +1,13 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Product : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+
+
+    }
+}
