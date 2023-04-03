55b426f8735e9ae3f3dbf497e4d563eb6702259b
Az1tchhh <98698562+Az1tchhh@users.noreply.github.com>
2023-04-03 15:13:49 +0600

    MainActivity changed, добавил интент на Регистрацию и Логин

--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.finalproject
 
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,80 +17,96 @@ class MainActivity : AppCompatActivity() {
     private lateinit var myLoginButton: Button
     private lateinit var myRegisterButton: Button
     private lateinit var myButtons: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         myLogo = findViewById(R.id.logoView)
         myLogoText = findViewById(R.id.logotextview)
+        myButtons = findViewById(R.id.buttonsFrameLayout)
         myLoginButton = findViewById(R.id.loginButtonWelcomePage)
         myRegisterButton = findViewById(R.id.registerButtonWelcomePage)
-        myButtons = findViewById(R.id.buttonsFrameLayout)
-        startLogo()
+
+
+        appearLogo()
         moveLogo()
-        startTextLogo()
+        appearTextLogo()
         startButtons()
+
+        myRegisterButton.setOnClickListener {
+            intent = Intent(this, Registration::class.java)
+            startActivity(intent)
+        }
+        myLoginButton.setOnClickListener {
+            intent = Intent(this, Authorisation::class.java)
+            startActivity(intent)
+        }
+
+    }
+    private val durationOfLogo: Long = 5000
+    private val durationOfText: Long = 2000
+    private val durationOfAnimUp: Long = 1500
+    private val durationOfAnimDown: Long = 500
+    private val durationOfAppearText: Long = 3000
+    private val durationOfAppearButtons: Long = 1000
+
+    private val delayOfLogoToMoveUp: Long = 5500
+    private val delayOfLogoToMoveDown: Long = delayOfLogoToMoveUp + 1500
+    private val delayOfTextToAppear: Long = delayOfLogoToMoveDown
+    private val delayOfButtonsToAppear: Long = delayOfTextToAppear + 1000
+    private fun appearLogo(){
+        myLogo.alpha = 0f
+        myLogo.visibility = View.VISIBLE
+        val animatorLogo = ObjectAnimator.ofFloat(myLogo, "alpha", 0f, 1f)
+        animatorLogo.duration = durationOfLogo
+        animatorLogo.start()
     }
 
-    private fun startTextLogo(){
+    private fun moveLogo(){
+        val animateTextUp = ObjectAnimator.ofFloat(myLogoText,"translationY", 0f, -700f)
+        animateTextUp.duration = 1
+        animateTextUp.start()
+
+        val animateButtonsUp = ObjectAnimator.ofFloat(myButtons,"translationY", 0f, -700f)
+        myLogoText.alpha = 0f
+        animateButtonsUp.duration = 1
+        animateButtonsUp.start()
+
+        val animateUp = ObjectAnimator.ofFloat(myLogo, "translationY", 0f, -750f)
+        animateUp.duration = durationOfAnimUp // 1000
+        animateUp.startDelay = delayOfLogoToMoveUp // 5000
+        animateUp.start()
+
+        val animateDown = ObjectAnimator.ofFloat(myLogo, "translationY", -750f, -700f)
+        animateDown.startDelay = delayOfLogoToMoveDown //7000
+        animateDown.duration = durationOfAnimDown //500
+        animateDown.start()
+    }
+    private fun appearTextLogo(){
         myLogoText.alpha = 0f
         val animator = ValueAnimator.ofFloat(-150f, 0f).apply {
-            duration = 2000
+            duration = durationOfText
             addUpdateListener { animation ->
                 val value = animation.animatedValue as Float
                 myLogoText.translationX = value
             }
         }
+        animator.startDelay = delayOfTextToAppear
 
-        val animatorLogoAppear = ObjectAnimator.ofFloat(myLogoText, "alpha", 0f, 1f)
-        animatorLogoAppear.duration = 3000
-        animatorLogoAppear.startDelay = 7000
+        val animatorTextAppear = ObjectAnimator.ofFloat(myLogoText, "alpha", 0f, 1f)
+        animatorTextAppear.duration = durationOfAppearText
+        animatorTextAppear.startDelay = delayOfTextToAppear
 
-
-        animator.startDelay = 7000
         animator.start()
-        animatorLogoAppear.start()
-
-
-
+        animatorTextAppear.start()
 
     }
-    private fun moveLogo(){
-
-        val animateUp = ObjectAnimator.ofFloat(myLogo, "translationY", 0f, -750f)
-        val animateTextUp = ObjectAnimator.ofFloat(myLogoText,"translationY", 0f, -700f)
-        val animateButtonsUp = ObjectAnimator.ofFloat(myButtons,"translationY", 0f, -700f)
-        myLogoText.alpha = 0f
-
-        val dur : Long = 1500 //mlsec
-
-        animateTextUp.duration = 1
-        animateButtonsUp.duration = 1
-        animateButtonsUp.start()
-        animateTextUp.start()
-        animateUp.duration = dur // 1000
-        animateUp.startDelay = 5500
-        animateUp.start()
-
-        val animateDown = ObjectAnimator.ofFloat(myLogo, "translationY", -750f, -700f)
-        animateDown.startDelay = 7000 // 1000
-        animateDown.duration = dur/2 // 500
-
-        animateDown.start()
-    }
-    private fun startLogo(){
-        myLogo.alpha = 0f
-        myLogo.visibility = View.VISIBLE
-        val animatorLogo = ObjectAnimator.ofFloat(myLogo, "alpha", 0f, 1f)
-        animatorLogo.duration = 5000
-        animatorLogo.start()
-    }
-
     private fun startButtons(){
         myButtons.alpha = 0f
         val animatorButtons = ObjectAnimator.ofFloat(myButtons, "alpha", 0f, 1f)
-        animatorButtons.duration = 1000
-        animatorButtons.startDelay = 8000
+        animatorButtons.duration = durationOfAppearButtons
+        animatorButtons.startDelay = delayOfButtonsToAppear
         animatorButtons.start()
 
     }
