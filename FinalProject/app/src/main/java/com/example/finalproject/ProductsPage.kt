454e646efa716da41d7458e9d972c3a676cd18fd
Az1tchhh <98698562+Az1tchhh@users.noreply.github.com>
2023-04-04 11:52:25 +0600

    little pages

--- FinalProject/app/src/main/java/com/example/finalproject/ProductsPage.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/ProductsPage.kt
@@ -3,7 +3,7 @@ package com.example.finalproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ProductPage : AppCompatActivity() {
+class ProductsPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_page)
