bab451d4fbd8bda0eaa8d0792491fabc3b972118
Az1tchhh <98698562+Az1tchhh@users.noreply.github.com>
2023-04-04 14:11:23 +0600

    kkk

--- FinalProject/app/src/main/java/com/example/finalproject/ProductsPage.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/ProductsPage.kt
@@ -2,12 +2,31 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.finalproject.databinding.ActivityProductPageBinding
+
 
 class ProductsPage : AppCompatActivity() {
+    lateinit var binding: ActivityProductPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_page)
+        binding = ActivityProductPageBinding.inflate(layoutInflater)
+        val products_page = binding.root
+        setContentView(products_page)
 
-
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            menuInflater.inflate(R.menu.menu_products_page, menu)
+            return true
+        }
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.action_settings -> {
+                    // Handle settings button click
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
