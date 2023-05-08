07b27a7aa9e0560b1ab316e6fb2608b50328ff5d
Az1tchhh <azamatbazarbai007@gmail.com>
2023-05-08 22:25:27 +0600

    фыв

--- AndroidBackk/api/serializers.py
+++ AndroidBackk/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Product, Commentary
+from .models import Category, Product, Commentary, Order
 
 from django.contrib.auth.models import User
 
@@ -27,5 +27,29 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Commentary
-        fields = ('id','user', 'text', 'created_at', 'product')
+        fields = ('id', 'user', 'text', 'created_at', 'product')
         read_only_fields = ('id', 'created_at')
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = UserField()
+
+    class Meta:
+        model = Order
+        fields = ('id', 'user', 'products')
+        read_only_fields = ('id', 'user')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
