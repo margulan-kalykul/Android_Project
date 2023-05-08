3b21501207eece1ad182bcbe546d99faf44fe857
margulan-kalykul <m_kalykul@kbtu.kz>
2023-05-08 20:04:24 +0600

    Made activity_product layout

--- FinalProject/app/src/main/java/com/example/finalproject/Product.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/Product.kt
@@ -2,11 +2,15 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finalproject.databinding.ActivityProductBinding
 
 class Product : AppCompatActivity() {
+    lateinit var binding: ActivityProductBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product)
+        binding = ActivityProductBinding.inflate(layoutInflater)
+        val productLayout = binding.root
+        setContentView(productLayout)
 
 
     }
