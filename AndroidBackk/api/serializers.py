68c74a8fc5ff6bc8def999388bdbd2b253c74457
Az1tchhh <azamatbazarbai007@gmail.com>
2023-05-08 19:11:19 +0600

    modified

--- AndroidBackk/api/serializers.py
+++ AndroidBackk/api/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 from .models import Category, Product, Commentary
-from django.contrib.auth.models import User
-
 class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -14,14 +12,9 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username')
-
-
 class CommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = serializers.StringRelatedField()
+
     class Meta:
         model = Commentary
         fields = ('user', 'text', 'created_at', 'product')
