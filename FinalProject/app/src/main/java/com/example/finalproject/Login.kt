9318f438ec306225c0ca31322648f46b1966628c
Az1tchhh <azamatbazarbai007@gmail.com>
2023-05-10 17:16:44 +0600

    asd

--- FinalProject/app/src/main/java/com/example/finalproject/Login.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/Login.kt
@@ -1,22 +1,30 @@
 package com.example.finalproject
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.finalproject.databinding.ActivityLoginBinding
-import com.example.finalproject.interfaces.Token
 import com.example.finalproject.interfaces.UserLogin
+import com.example.finalproject.interfaces.users.UserId
 import com.example.finalproject.retrofit.RetrofitHelper
 import com.example.finalproject.service.ServerAPI
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
+import retrofit2.HttpException
 
 class Login : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
+    val client = OkHttpClient.Builder().build()
+    val retrofit = RetrofitHelper(client)
+    val rf = retrofit.getInstance()
+    val itemAPI = rf.create(ServerAPI::class.java)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -24,41 +32,61 @@ class Login : AppCompatActivity() {
         setContentView(loginPage)
 
 
-        val client = OkHttpClient.Builder().build()
-        val retrofit = RetrofitHelper(client)
-        val rf = retrofit.getInstance()
-        val itemAPI = rf.create(ServerAPI::class.java)
-
         val loginButton = binding.buttonLogin
         loginButton.setOnClickListener{
+
+
             val username = binding.usernameField.text.toString()
             val password = binding.passwordField.text.toString()
             val credentials = UserLogin(username, password)
 
+            if (checkValidness(credentials)){
+                CoroutineScope(Dispatchers.IO).launch {
+                    if( isUserExist(credentials) ){
+                        val userId: UserId = getUserId(username)
 
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    val listTokens = itemAPI.postUserCredentials(credentials)
-                    Log.d("data", listTokens.toString())
-                } catch (e: retrofit2.HttpException){
-                    showToast(applicationContext, "You may not have registered yet")
+                        val intent = Intent(applicationContext, PersonalCabinet::class.java)
+                        intent.putExtra("userId", userId.id)
+                        intent.putExtra("userName", username)
+                        startActivity(intent)
+                    }
                 }
-
             }
-
-
         }
     }
-    private fun isUserExist(token: Token): Boolean{
-        if (token != null){
-            showToast(applicationContext, "Authorization success!")
-            return true
+    private fun checkValidness(credentials: UserLogin): Boolean{
+        if(credentials.username.isEmpty() or credentials.password.isEmpty()){
+            runOnUiThread {
+                showToast(applicationContext, "Fill all credentials")
+            }
+            return false
         }
-        showToast(applicationContext, "User need to register")
-        return false
+        return true
     }
 
+    private suspend fun isUserExist(credentials: UserLogin): Boolean{
+        try {
+            val listTokens = this.itemAPI.postUserCredentials(credentials)
+            Log.d("data", listTokens.toString())
+        } catch (e: HttpException){
+            runOnUiThread {
+                showToast(applicationContext, "No active account found with the given credentials")
+            }
+            return false
+        }
+
+        return true
+    }
+
+    private suspend fun getUserId(username: String): UserId {
+        Log.d("data", this.itemAPI.getUser(username).toString())
+        return this.itemAPI.getUser(username)
+    }
     private fun showToast(context: Context, text: String) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun infoAboutError(){
+
     }
 }
