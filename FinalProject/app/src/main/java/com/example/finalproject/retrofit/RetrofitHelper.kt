7fbd93a5486847fdaaaac018675649135cb07c36
Az1tchhh <azamatbazarbai007@gmail.com>
2023-05-10 17:27:39 +0600

    asd

--- FinalProject/app/src/main/java/com/example/finalproject/retrofit/RetrofitHelper.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/retrofit/RetrofitHelper.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitHelper(var client: OkHttpClient) {
-    var url = "https://00f9-146-120-202-175.ngrok-free.app/api/"
+    var url = "https://59a7-5-251-200-170.ngrok-free.app/api/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(url).client(client)
